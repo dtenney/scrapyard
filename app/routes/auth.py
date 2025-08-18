bb5a3bde452726120d05429528c0acf9cf14304e
Scrap Yard Admin <admin@scrapyard.local>
2025-08-18 15:00:42 -0400

    Add missing current_user import to auth routes

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, request, redirect, url_for, flash
-from flask_login import login_user, logout_user, login_required
+from flask_login import login_user, logout_user, login_required, current_user
 from app.models.user import User
 from app import db
 
